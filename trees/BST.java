0b5eeb4cff230ba7f7ebfa06c0be835b50465153
Mayank Choudhary <choudharymayank006@gmail.com>
2026-01-24 11:34:57 +0530

    Created Binary Search Tree, added insert and inorder traversal function

---
+++ trees/BST.java
@@ -0,0 +1,37 @@
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int x) {
+        data = x;
+        left = right = null;
+    }
+}
+
+public class BST {
+    
+    Node insert (Node root, int val) {
+
+        if (root == null) {
+            return root = new Node(val);
+        }
+        if (root.data > val) {
+            root.left = insert (root.left, val);
+        }
+        else {
+            root.right = insert (root.right, val);
+        }
+        return root;
+    }
+
+    void inorder (Node root) { // To check if binary search tree is properly created - Elements - Sorted Order - Increasing
+
+        if (root != null) {
+            inorder(root.left);
+            System.out.println(root.data + " -> ");
+            inorder(root.right);
+        }
+    } 
+}
